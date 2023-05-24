464ca2e45b1b6c66fc996945270e62f1a4c99e69
Samuel Otton <ottonsam@gmail.com>
2023-05-24 13:36:19 -0300

    update: testes  enesimo elemento, media e posicao

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -50,3 +50,25 @@ class TreeTest(TestCase):
 
         tree = BinarySearchTree.from_list([5, 3, 8, 6, 10, 9, 2, 1, 4])
         self.assertEqual(tree.pre_ordem(), "5 3 2 1 4 8 6 10 9")
+
+    def test_enesimoElemento(self):
+        tree = BinarySearchTree.from_list([5, 3, 8, 6, 9])
+        self.assertEqual(tree.enesimoElemento(2), 5)
+
+        tree = BinarySearchTree.from_list([5, 3, 8, 6, 10, 9, 2, 1, 4])
+        self.assertEqual(tree.enesimoElemento(1), 1)
+
+    def test_media(self):
+        tree = BinarySearchTree.from_list([5, 3, 8, 6, 9])
+        self.assertEqual(tree.media(9), 9.0)
+
+        self.assertEqual(tree.media(5), 6.2)
+
+        
+    def test_posicao(self):
+        tree = BinarySearchTree.from_list([5, 3, 8, 6, 9])
+        self.assertEqual(tree.posicao(5), 2)
+
+        self.assertEqual(tree.posicao(3), 1)
+
+        self.assertEqual(tree.posicao(9), 5)
